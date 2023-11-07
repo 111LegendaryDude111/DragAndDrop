c15516938ecb95e554e2dc559466a96445503896
David <davidgoingforward@gmail.com>
2023-11-07 22:33:40 +0300

    (DragAndDrop): fix drags function and project design

--- src/components/Canvas/Ticket/utils.ts
+++ src/components/Canvas/Ticket/utils.ts
@@ -1,7 +1,9 @@
 export const onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
   const target = e.currentTarget;
   target.style.boxShadow = `none`;
-  target.style.transform = ` translate(${e.pageX - 170}px,${e.pageY}px)`;
+  target.style.transform = ` translate(${e.pageX - target.offsetLeft}px,${
+    e.pageY - target.offsetTop
+  }px)`;
 };
 
 export const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
